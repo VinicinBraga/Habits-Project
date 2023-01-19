cf49f443933918faf6b38ea49068ba13bef88738
VinicinBraga <vinicius@bragamatta.com.br>
2023-01-19 13:11:01 -0300

    HabitDay and SummaryTable Components

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import HabitDay from './HabitDay';
+import genereteDates from '../utils/generete-dates';
+
+const weekDays = [
+  'S','M','T','W','T','F','S'
+]
+
+const summaryDates = genereteDates()
+const minimumNumberOfDays = 18 * 7
+const amountDaysToFill = minimumNumberOfDays - summaryDates.length
+
+const SummaryTable = () => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => {
+          return (
+            <div key={index} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3 cursor-pointer">
+        {summaryDates.map((date, index) => {
+          return <HabitDay key={index}/>
+        })}
+        
+        {amountDaysToFill > 0 && Array.from({ length: amountDaysToFill }).map((_, i) => {
+          return <div key={i} className="w-10 h10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default SummaryTable
---
+++ web/src/utils/generete-dates.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import dayjs from 'dayjs'
+
+const genereteDates = () => {
+
+  const firstDayofYear = dayjs().startOf('year')
+  const today = new Date()
+
+  const dates = []
+  let compareDate = firstDayofYear
+
+  while (compareDate.isBefore(today)) {
+    dates.push(compareDate.toDate())
+    compareDate = compareDate.add(1, 'day')
+  }
+  return dates
+  
+}
+
+export default genereteDates
